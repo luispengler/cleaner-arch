2c50b97a205ea84aadf94ee5d083c65d662f8914
luispengler <71613402+luispengler@users.noreply.github.com>
2020-09-25 12:13:49 -0400

    Update cleaning.sh

--- cleaning.sh
+++ cleaning.sh
@@ -4,15 +4,17 @@
 
 sudo pacman -Syy;
 
-## the next command will delete your package cache, but only the uninstalled stuff ##
+echo "the next command will delete your package cache, but only the uninstalled stuff"
 
 sudo pacman -Sc;
 
-## you don't need this every time ## 
+echo "you don't need this every time"
 
 sudo pacman -Qtdq && sudo pacman -Rns $(pacman -Qtdq)
 
 ## cleaning up your cache ##
 
-sudo du -sh ~/.cache/ && rm -rf ~/.cache/*
+sudo du -sh ~/.cache/ && rm -rf ~/.cache/* && clear
+
+echo "This is the end, bitch. UHAHAHAHAHAHAHAHAHAHAHA"
 
